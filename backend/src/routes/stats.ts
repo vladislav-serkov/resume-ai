86cf54577c228d08133db9ae119207210d5af882
vserkov <vserkov@mtsbank.ru>
2025-08-20 17:49:04 +0300

    Revert "auto-commit for c13078d1-e80f-4284-8679-ba1109c73499"
    This reverts commit fb6b5af7218c6a9356e17a9ba1b69ed46cb87dbd.

--- backend/src/routes/stats.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { StatsController } from '../controllers/statsController';
-
-const router = Router();
-
-router.get('/', StatsController.getUserStats);
-
-export default router;
